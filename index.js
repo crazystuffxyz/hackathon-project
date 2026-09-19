62f416d093e8bbe91a0b6a1d934afaa3586fe580
crazystuffxyz <shayaanhooda@gmail.com>
2026-09-19 16:38:50 -0400

    I fixed something or added something (committed quickly)

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import {join} from "node:path";
 const app = express();
+const __dirname = import.meta.dirname;
+
+app.use(express.static(join(__dirname, "static")));
 app.use((req, res) => {
-    res.send("Nothing here yet");
+    res.sendFile(join(__dirname, "static/404.html"));
 })
 //Im stupid
 process.env.port = 8080;
 app.listen(process.env.port, () => {
-    console.log(`Does http://localhost${process.env.port} even work?`);
+    console.log(`Does http://localhost:${process.env.port} even work?`);
 })
