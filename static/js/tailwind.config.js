e8dd0bfd807eb30962930c2dcc9d97ca023ef1fd
crazystuffxyz <shayaanhooda@gmail.com>
2026-09-19 21:46:21 -0400

    I fixed something or added something (committed quickly)

--- static/js/tailwind.config.js
+++ static/js/tailwind.config.js
@@ -1,3 +1,32 @@
+window.tailwind = window.tailwind || {};
+
 window.tailwind.config = {
-    
+    theme: {
+        extend: {
+            colors: {
+                ink: "#17231d",
+                paper: "#f1ead9",
+                "paper-deep": "#e5dbc4",
+                field: "#294438",
+                sage: "#607662",
+                harvest: "#d4933a",
+                berry: "#8e3f45",
+                muted: "#756f61"
+            },
+
+fontFamily: {
+                display: [
+                    "Georgia",
+                    "Times New Roman",
+                    "serif"
+                ],
+
+sans: [
+                    "Trebuchet MS",
+                    "system-ui",
+                    "sans-serif"
+                ]
 }
+        }
+    }
+};
